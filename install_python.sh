89ef0db9d2cf77fe04d9af62aae419200dc989f9
Tammy <tammyebufegha@hotmail.com>
2020-03-12 16:56:59 -0400

    My bash files and notes

---
+++ install_python.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+cd /tmp
+wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz
+tar –xf Python-3.7.5.tgz
+cd python-3.7.5
+./configure ––enable–optimizations
+sudo make altinstall
+python3 ––version
